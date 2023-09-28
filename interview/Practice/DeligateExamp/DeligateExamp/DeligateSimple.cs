59b35f1f60a5f57ff19e9d28b30a60f907c36a36
brajesh1990 <kumarbrajesh464@gmail.com>
2023-09-28 20:34:17 +0530

    initial

---
+++ interview/Practice/DeligateExamp/DeligateExamp/DeligateSimple.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeligateExamp
+{
+    public delegate void Simpledel();
+   internal class DeligateSimple
+    {
+        static public void Simplemsg()
+        {
+            Console.WriteLine("Hellow I am from chd");
+        }
+
+    }
+}
